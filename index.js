70956fed56f8a73c07ac2fe0002a907cf53573a2
VisXhal06 <vishalsuthar2711@gmail.com>
2025-09-14 17:12:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const { z } = require("zod");
 
-mongoose.connect("")
+mongoose.connect("");
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
+    const requiredBody = z.object({
+        email: z.string().email(),
+        password: z.string().min(6).max(20),
+        name: z.string().min(3).max(10)
+    });
+    //const parsedData = requiredBody.Parse(req.body);
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Invalid Format"
+        });
+        return;
+    }
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
     
-    res.json({
-        message: "You are signed up"
-    })
+    let errorThrown = false;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 5);
+        console.log(hashedPassword);
+
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+    } catch(err) {
+        res.json({
+            message: "User already exists in Database"
+        });
+        errorThrown = true;
+        console.log(err);
+        return;
+    }
+    
+    if(!errorThrown){
+        res.json({
+            message: "You are signed up"
+        });
+    }
 });
 
 
@@ -31,11 +63,19 @@ app.post("/signin", async function(req, res) {
     const password = req.body.password;
 
     const response = await UserModel.findOne({
-        email: email,
-        password: password,
+        email: email
     });
 
-    if (response) {
+    if(!response) {
+        res.status(403).json({
+            message: "User not found or exists in Database"
+        });
+        return;
+    }
+
+    const passwordMatch = bcrypt.compare(password, response.password);
+
+    if (response && passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
         }, JWT_SECRET);
